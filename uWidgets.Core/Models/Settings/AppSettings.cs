a06311d325b63e4db16ca89dce295be7739cbefe
FitoDomik <sartelium@gmail.com>
2025-09-14 20:40:13 +0300

    Add files via upload

---
+++ uWidgets.Core/Models/Settings/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace uWidgets.Core.Models.Settings;
+
+/// <summary>
+/// Application settings, stored in <c>appsettings.json</c>.
+/// </summary>
+public record AppSettings(
+    Theme Theme, 
+    Theme[] Templates,
+    Layout Layout, 
+    Dimensions Dimensions, 
+    Region Region, 
+    bool RunOnStartup,
+    string? IgnoreUpdate);
---
+++ uWidgets.Core/Models/Settings/Theme.cs
@@ -0,0 +1,32 @@
+﻿namespace uWidgets.Core.Models.Settings;
+
+/// <summary>
+/// Application theme settings.
+/// </summary>
+/// <param name="DarkMode">
+/// Should the application use the dark mode
+/// <para><c>null</c> to use system settings</para>
+/// </param>
+/// <param name="AccentColor">
+/// Accent color in HEX format
+/// <para><c>null</c> to use system accent color</para>
+/// </param>
+/// <param name="OpacityLevel">
+/// Widget's background opacity level
+/// </param>
+/// <param name="Monochrome">
+/// Should the application use monochrome theme
+/// </param>
+/// <param name="UseNativeFrame">
+/// Should the application use native window frame
+/// </param>
+/// <param name="FontFamily">
+/// Font family to use
+/// </param>
+public record Theme(
+    bool? DarkMode, 
+    string? AccentColor, 
+    double OpacityLevel, 
+    bool Monochrome, 
+    bool UseNativeFrame, 
+    string FontFamily);
